ad8db1f340cb84962916ae09d57f96b11589e4eb
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 12:41:36 -0500

    Se agrega comentarios en inicio.component.spec.ts

--- src/app/Admin/inicio/inicio.component.spec.ts
+++ src/app/Admin/inicio/inicio.component.spec.ts
@@ -2,22 +2,29 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { InicioComponent } from './inicio.component';
 
+// Definición del bloque de pruebas para el componente "InicioComponent"
 describe('InicioComponent', () => {
-  let component: InicioComponent;
-  let fixture: ComponentFixture<InicioComponent>;
+  // Se declaran las variables necesarias para la prueba del componente
+  let component: InicioComponent;  // Variable que almacena el componente a probar
+  let fixture: ComponentFixture<InicioComponent>;  // Variable que contiene la referencia a la instancia de la vista del componente
 
+  // Configuración del entorno de pruebas antes de cada prueba individual
   beforeEach(async () => {
+    // Configura el módulo de pruebas que incluirá el componente a testear
     await TestBed.configureTestingModule({
-      imports: [InicioComponent]
+      imports: [InicioComponent]  // Importa el componente para la configuración del módulo de prueba
     })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(InicioComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      .compileComponents();  // Compila el componente
+
+    // Crea una instancia del componente para las pruebas
+    fixture = TestBed.createComponent(InicioComponent);  // Genera una instancia de la vista del componente
+    component = fixture.componentInstance;  // Obtiene la instancia del componente
+    fixture.detectChanges();  // Dispara la detección de cambios para aplicar las actualizaciones de Angular
   });
 
+  // Test: Verifica si el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    // Verifica si la instancia del componente fue creada correctamente
+    expect(component).toBeTruthy();  // Comprueba que el componente no sea nulo o indefinido
   });
 });
