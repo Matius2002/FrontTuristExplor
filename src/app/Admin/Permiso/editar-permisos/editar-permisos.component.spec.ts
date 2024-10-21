1f8ec4f55081fe61be4048571f364a5131a8d691
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 12:44:06 -0500

    Se agrega comentarios en editar-permisos.component.spec.ts

--- src/app/Admin/Permiso/editar-permisos/editar-permisos.component.spec.ts
+++ src/app/Admin/Permiso/editar-permisos/editar-permisos.component.spec.ts
@@ -2,22 +2,30 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditarPermisosComponent } from './editar-permisos.component';
 
+// Definición del bloque de pruebas para el componente "EditarPermisosComponent"
 describe('EditarPermisosComponent', () => {
-  let component: EditarPermisosComponent;
-  let fixture: ComponentFixture<EditarPermisosComponent>;
+  // Se declaran las variables necesarias para la prueba del componente
+  let component: EditarPermisosComponent;  // Variable que almacena el componente a probar
+  let fixture: ComponentFixture<EditarPermisosComponent>;  // Variable que contiene la referencia a la instancia de la vista del componente
 
+  // Configuración del entorno de pruebas antes de cada prueba individual
   beforeEach(async () => {
+    // Configura el módulo de pruebas que incluirá el componente a testear
     await TestBed.configureTestingModule({
-      imports: [EditarPermisosComponent]
+      imports: [EditarPermisosComponent]  // Importa el componente para la configuración del módulo de prueba
     })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(EditarPermisosComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      .compileComponents();  // Compila el componente
+
+    // Crea una instancia del componente para las pruebas
+    fixture = TestBed.createComponent(EditarPermisosComponent);  // Genera una instancia de la vista del componente
+    component = fixture.componentInstance;  // Obtiene la instancia del componente
+    fixture.detectChanges();  // Dispara la detección de cambios para aplicar las actualizaciones de Angular
   });
 
+  // Test: Verifica si el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    // Verifica si la instancia del componente fue creada correctamente
+    expect(component).toBeTruthy();  // Comprueba que el componente no sea nulo o indefinido
   });
 });
+
