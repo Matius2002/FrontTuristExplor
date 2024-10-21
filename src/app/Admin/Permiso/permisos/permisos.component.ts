99d355886089d49f1b8ac95750f8310f446f58c9
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 12:53:00 -0500

    Se agrega comentarios en PermisosComponent

--- src/app/Admin/Permiso/permisos/permisos.component.ts
+++ src/app/Admin/Permiso/permisos/permisos.component.ts
@@ -2,7 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {PermisosService} from "../permisos.service";
 
-interface  Rol {
+// Interfaces para definir las estructuras de datos de Rol y Permisos
+interface Rol {
   id: number;
   rolName: string;
 }
@@ -10,20 +11,21 @@ interface  Rol {
 interface Permisos {
   id: number;
   name: string;
-
 }
+
+// Decorador Component para definir el componente PermisosComponent
 @Component({
-  providers: [HttpClient, PermisosService],
-  selector: 'app-permisos',
-  standalone: true,
-  imports: [
-
-  ],
-  templateUrl: './permisos.component.html',
-  styleUrl: './permisos.component.css'
+  providers: [HttpClient, PermisosService],  // Servicios inyectados: HttpClient y PermisosService
+  selector: 'app-permisos',  // Selector del componente (en el HTML se utilizará como <app-permisos>)
+  standalone: true,  // El componente es independiente y no depende de otros módulos
+  imports: [],  // Aquí puedes incluir módulos adicionales si es necesario
+  templateUrl: './permisos.component.html',  // Ruta al archivo de plantilla HTML del componente
+  styleUrl: './permisos.component.css'  // Ruta al archivo de estilos CSS del componente
 })
-export class PermisosComponent implements  OnInit{
+export class PermisosComponent implements OnInit {
+  // El método ngOnInit es el ciclo de vida de Angular que se ejecuta cuando el componente es inicializado
   ngOnInit(): void {
+    // Aquí se pueden realizar inicializaciones o llamadas a servicios
   }
-
 }
+
