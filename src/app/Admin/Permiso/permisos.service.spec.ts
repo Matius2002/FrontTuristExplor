251cb6fcb01cd5bc36c1fef27907463f518c7eb3
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 12:54:48 -0500

    Se agrega comentarios en permisos.service.spec.ts

--- src/app/Admin/Permiso/permisos.service.spec.ts
+++ src/app/Admin/Permiso/permisos.service.spec.ts
@@ -2,15 +2,18 @@ import { TestBed } from '@angular/core/testing';
 
 import { PermisosService } from './permisos.service';
 
+// Describe el conjunto de pruebas para PermisosService
 describe('PermisosService', () => {
-  let service: PermisosService;
+  let service: PermisosService;  // Variable para almacenar la instancia del servicio
 
+  // Antes de cada prueba, se configura el módulo de pruebas
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(PermisosService);
+    TestBed.configureTestingModule({});  // Configura el TestBed, que es un contenedor de pruebas de Angular
+    service = TestBed.inject(PermisosService);  // Inyecta el servicio en la variable service
   });
 
+  // Prueba básica para verificar que el servicio se ha creado correctamente
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy();  // Espera que la instancia del servicio sea válida
   });
 });
