a724d72d596fbe3de67c184fd48426a45ae26457
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 12:46:04 -0500

    Se agrega comentarios en EditarPermisosComponent

--- src/app/Admin/Permiso/editar-permisos/editar-permisos.component.ts
+++ src/app/Admin/Permiso/editar-permisos/editar-permisos.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+// Decorador @Component: Define un componente Angular con sus metadatos
 @Component({
-  selector: 'app-editar-permisos',
-  standalone: true,
-  imports: [],
-  templateUrl: './editar-permisos.component.html',
-  styleUrl: './editar-permisos.component.css'
-})
-export class EditarPermisosComponent {
+  // Selector utilizado para identificar el componente en el HTML
+  selector: 'app-editar-permisos',  // Nombre que se usará para incluir este componente en otros componentes o plantillas HTML
 
+  // Indica que este componente es autónomo, lo que significa que puede funcionar de forma independiente sin depender de otros módulos
+  standalone: true,
+
+  // Los módulos importados necesarios para este componente (en este caso está vacío, lo que significa que no hay módulos adicionales importados)
+  imports: [],
+
+  // Ruta al archivo de plantilla HTML que define la estructura visual del componente
+  templateUrl: './editar-permisos.component.html',  // Enlace a la plantilla HTML
+
+  // Ruta al archivo de estilo CSS que define los estilos visuales del componente
+  styleUrl: './editar-permisos.component.css'  // Enlace a los estilos CSS
+})
+
+// Definición de la clase EditarPermisosComponent
+export class EditarPermisosComponent {
+  // Esta clase está vacía por ahora, pero puede contener lógica de negocio o métodos para el comportamiento del componente
 }
+
