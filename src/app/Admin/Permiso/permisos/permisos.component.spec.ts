b5e7f240563977d4a764b57c1544b123693b5aff
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 12:51:13 -0500

    Se agrega comentarios en permisos.component.spec.ts

--- src/app/Admin/Permiso/permisos/permisos.component.spec.ts
+++ src/app/Admin/Permiso/permisos/permisos.component.spec.ts
@@ -2,22 +2,33 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PermisosComponent } from './permisos.component';
 
+// Define el conjunto de pruebas para el componente PermisosComponent
 describe('PermisosComponent', () => {
+  // Define las variables para el componente y el fixture que lo prueba
   let component: PermisosComponent;
   let fixture: ComponentFixture<PermisosComponent>;
 
+  // Antes de cada prueba
   beforeEach(async () => {
+    // Configura el entorno de pruebas de Angular TestBed
     await TestBed.configureTestingModule({
+      // Importa el componente PermisosComponent para la prueba
       imports: [PermisosComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();  // Compila los componentes
+
+    // Crea una instancia del componente a probar
     fixture = TestBed.createComponent(PermisosComponent);
+    // Obtiene la instancia del componente
     component = fixture.componentInstance;
+    // Dispara la detección de cambios, asegurando que el componente se haya renderizado correctamente
     fixture.detectChanges();
   });
 
+  // Test para verificar que el componente se crea correctamente
   it('should create', () => {
+    // Comprobamos que la instancia del componente se ha creado correctamente
     expect(component).toBeTruthy();
   });
 });
+
