8bb5d472bc85dee524e7c30beb6a57093bf6f860
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 11:50:28 -0500

    Eliminar FrontTuristExplor

--- src/app/Admin/conoce-girardot/conoce-girardot.component.spec.ts
+++ src/app/Admin/conoce-girardot/conoce-girardot.component.spec.ts
@@ -2,22 +2,32 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ConoceGirardotComponent } from './conoce-girardot.component';
 
+// Descripción de la prueba para el componente ConoceGirardotComponent
 describe('ConoceGirardotComponent', () => {
-  let component: ConoceGirardotComponent;
-  let fixture: ComponentFixture<ConoceGirardotComponent>;
+  // Definición de las variables que se utilizarán en las pruebas
+  let component: ConoceGirardotComponent;  // Variable que contendrá la instancia del componente
+  let fixture: ComponentFixture<ConoceGirardotComponent>;  // Fixture que ayudará a crear el componente y manipular su DOM
 
+  // Ejecuta antes de cada prueba
   beforeEach(async () => {
+    // Configura el módulo de pruebas
     await TestBed.configureTestingModule({
+      // Importa el componente que se va a probar en el módulo
       imports: [ConoceGirardotComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();  // Compila los componentes necesarios para las pruebas
+
+    // Crea una instancia del componente y lo asigna a la variable fixture
     fixture = TestBed.createComponent(ConoceGirardotComponent);
+    // Asigna la instancia del componente a la variable component
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    // Realiza las actualizaciones del DOM del componente
+    fixture.detectChanges();  // Detecta los cambios en el componente
   });
 
+  // Prueba que el componente se cree correctamente
   it('should create', () => {
+    // Asegura que la instancia del componente exista (es decir, que el componente se haya creado correctamente)
     expect(component).toBeTruthy();
   });
 });
