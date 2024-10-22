28d65335da9e8507fcdb5ed6d6b321bee396a7f3
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-22 17:44:55 -0500

    Se agregan comentarios en reporte-usuarios.component.ts

--- src/app/Reportes/reporte-usuarios/reporte-usuarios.component.ts
+++ src/app/Reportes/reporte-usuarios/reporte-usuarios.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 
+// Decorador @Component que define las propiedades de configuración del componente
 @Component({
-  selector: 'app-reporte-usuarios',
-  standalone: true,
-  imports: [],
-  templateUrl: './reporte-usuarios.component.html',
-  styleUrl: './reporte-usuarios.component.css'
+  selector: 'app-reporte-usuarios', // Nombre del selector que se usará en el HTML para instanciar este componente
+  standalone: true, // Indica que este componente es independiente y puede ser usado sin estar dentro de un módulo
+  imports: [], // Importaciones de otros módulos o componentes (en este caso no hay importaciones)
+  templateUrl: './reporte-usuarios.component.html', // Ruta al archivo HTML de la plantilla del componente
+  styleUrl: './reporte-usuarios.component.css' // Ruta al archivo de estilos CSS del componente
 })
 export class ReporteUsuariosComponent {
-
+  // Aquí se pueden definir propiedades, métodos y lógica del componente
+  // En este momento, el componente está vacío
 }
