524d2d2030053463172e946698534eb90da35013
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-22 18:03:13 -0500

    Se agregan comentarios en reporte.service.ts

--- src/app/Reportes/report.service.ts
+++ src/app/Reportes/report.service.ts
@@ -5,12 +5,15 @@ import {Observable} from "rxjs";
 import {UsuarioService} from "../Admin/Usuarios/usuario.service";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Indica que el servicio se proporciona en el nivel raíz
 })
 export class ReportService {
-  dynamicHost = entornos.dynamicHost;
-  private baseUrl: string = `http://${this.dynamicHost}/api`;
+  dynamicHost = entornos.dynamicHost; // Obtiene el host dinámico desde las configuraciones de entornos
+  private baseUrl: string = `http://${this.dynamicHost}/api`; // Define la URL base para las API
+
+  // Constructor que inyecta HttpClient y UsuarioService
   constructor(private http: HttpClient, private usuarioService: UsuarioService) { }
+
   /**
    * Obtiene la URL del reporte según el tipo y el formato.
    * @param reportType El tipo de reporte (usuarios, visitas, comentarios)
@@ -19,7 +22,8 @@ export class ReportService {
    */
   //***************************************************************//
   getReportUrl(reportType: string, format: string): Observable<any> {
-    let endpoint = '';
+    let endpoint = ''; // Inicializa la variable endpoint
+    // Determina el endpoint basado en el tipo de reporte
     if (reportType === 'usuarios') {
       endpoint = `${this.baseUrl}/reportes/usuarios/${format}`;
     } else if (reportType === 'visitas') {
@@ -27,27 +31,20 @@ export class ReportService {
     } else if (reportType === 'comentarios') {
       endpoint = `${this.baseUrl}/reportes/comentarios/${format}`;
     }
+    // Realiza una solicitud GET al endpoint y espera una respuesta de tipo blob
     return this.http.get(endpoint, { responseType: 'blob' });
   }
   //***************************************************************//
 
+  // Métodos para obtener reportes de visitas en PDF y Excel.
 
-
-
-
-
-
-
-
-
-
-  // REPORTE DE VISITAS EN PDF Y EXCEL.
   /**
    * Descarga el reporte de visitas en formato PDF.
    * @returns Observable con el archivo PDF.
    */
   getVisitasPdf(): Observable<Blob> {
-    const url = `${this.baseUrl}/reportes/visitas/pdf`;
+    const url = `${this.baseUrl}/reportes/visitas/pdf`; // Define la URL del reporte en PDF
+    // Realiza una solicitud GET y espera un blob como respuesta
     return this.http.get(url, { responseType: 'blob' });
   }
 
@@ -56,8 +53,8 @@ export class ReportService {
    * @returns Observable con el archivo Excel.
    */
   getVisitasExcel(): Observable<Blob> {
-    const url = `${this.baseUrl}/reportes/visitas/excel`;
+    const url = `${this.baseUrl}/reportes/visitas/excel`; // Define la URL del reporte en Excel
+    // Realiza una solicitud GET y espera un blob como respuesta
     return this.http.get(url, { responseType: 'blob' });
   }
-
 }
