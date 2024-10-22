e6a044cfe4ddb37eb6b74bbd8dea09f2f95601da
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 20:55:57 -0500

    Se agrega comentarios en reset-password.component.ts

--- src/app/Admin/Usuarios/login/reset-password/reset-password.component.ts
+++ src/app/Admin/Usuarios/login/reset-password/reset-password.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 
+// Decorador @Component que define el metadato del componente
 @Component({
-  selector: 'app-reset-password',
-  standalone: true,
-  imports: [],
-  templateUrl: './reset-password.component.html',
-  styleUrl: './reset-password.component.css'
+  selector: 'app-reset-password',  // Define el selector que se usará para insertar este componente en HTML
+  standalone: true,  // Indica que este componente es independiente, no requiere declararse en un módulo
+  imports: [],  // Aquí se especificarían los módulos que el componente necesita importar (vacío por ahora)
+  templateUrl: './reset-password.component.html',  // Ruta del archivo de la plantilla HTML asociada
+  styleUrl: './reset-password.component.css'  // Ruta del archivo de estilos CSS específico para este componente
 })
 export class ResetPasswordComponent {
+  // Aquí se podría definir la lógica del componente, como propiedades y métodos.
 
+  // El componente actualmente no tiene ninguna lógica implementada.
 }
+
