83cd32f949a968095ff363156f73cf16978cbcdf
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 20:30:27 -0500

    Se agrega comentarios en EditarUsuariosComponent

--- src/app/Admin/Usuarios/editar-usuarios/editar-usuarios.component.ts
+++ src/app/Admin/Usuarios/editar-usuarios/editar-usuarios.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
+  // Definición del selector para usar este componente en el HTML
   selector: 'app-editar-usuarios',
+
+  // Declaración de este componente como un componente independiente (standalone)
+  // Esto significa que no forma parte de ningún módulo, se puede usar directamente.
   standalone: true,
+
+  // Importaciones necesarias para este componente. Aquí está vacío, pero se podrían importar
+  // otros módulos de Angular como formularios, servicios, etc.
   imports: [],
+
+  // Ruta al archivo HTML que define la vista (plantilla) del componente
   templateUrl: './editar-usuarios.component.html',
-  styleUrl: './editar-usuarios.component.css'
+
+  // Ruta al archivo CSS que define los estilos específicos del componente
+  styleUrls: ['./editar-usuarios.component.css']
 })
 export class EditarUsuariosComponent {
-
+  // Aquí se pueden definir propiedades, métodos y lógica del componente
 }
