b7f45cafd0b15b057acb5cca21008b24b36a76e4
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-22 18:05:04 -0500

    Se agregan comentarios en visit-service.service.spec.ts

--- src/app/Reportes/visit-service.service.spec.ts
+++ src/app/Reportes/visit-service.service.spec.ts
@@ -3,14 +3,16 @@ import { TestBed } from '@angular/core/testing';
 import { VisitService } from './visit-service.service';
 
 describe('VisitServiceService', () => {
-  let service: VisitService;
+  let service: VisitService; // Declara una variable para el servicio VisitService
 
+  // Configuración inicial antes de cada prueba
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(VisitService);
+    TestBed.configureTestingModule({}); // Configura el TestBed para el módulo de prueba
+    service = TestBed.inject(VisitService); // Inyecta el servicio VisitService
   });
 
+  // Prueba para verificar si el servicio se crea correctamente
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy(); // Verifica que el servicio exista
   });
 });
