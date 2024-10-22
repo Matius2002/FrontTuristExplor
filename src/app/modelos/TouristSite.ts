23a15135a651f32f4f12aef763224b9ef08df13e
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-22 16:53:03 -0500

    Se agregan comentarios en TouristSite.ts

--- src/app/modelos/TouristSite.ts
+++ src/app/modelos/TouristSite.ts
@@ -1,8 +1,11 @@
+// tourist-site.model.ts
+
+// Define la interfaz TouristSite
 export interface TouristSite {
-  id?: number;
-  title: string;
-  content: string;
-  tourismType: string;
-  latitude: number;
-  longitude: number;
+  id?: number; // ID opcional del sitio turístico
+  title: string; // Título del sitio turístico
+  content: string; // Descripción del sitio turístico
+  tourismType: string; // Tipo de turismo (ej. cultural, gastronómico, etc.)
+  latitude: number; // Latitud de la ubicación del sitio turístico
+  longitude: number; // Longitud de la ubicación del sitio turístico
 }
