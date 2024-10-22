2c6e6bbe16e6e17e4fa177a8c6f096d8597146b6
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-22 16:23:33 -0500

    Se agregan comentarios en entorno.ts

--- src/app/Entorno/entornos.ts
+++ src/app/Entorno/entornos.ts
@@ -1,7 +1,10 @@
-export const entornos =
-  {
-    production: false,
+// Exporta una constante llamada 'entornos' que contiene la configuración del entorno de la aplicación
+export const entornos = {
+  // Indica si la aplicación está en modo de producción
+  production: false, // 'false' significa que está en modo de desarrollo
 
-    dynamicHost: 'localhost:8080',
-  };
+  // Define la dirección del host dinámico donde se ejecuta el backend
+  dynamicHost: 'localhost:8080', // URL del backend en el entorno local
+};
+
 
