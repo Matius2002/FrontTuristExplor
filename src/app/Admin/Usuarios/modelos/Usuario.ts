301c724408ae6e142cfa463268e43637717f174c
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 21:10:40 -0500

    Se agrega comentarios en Usuario.ts

--- src/app/Admin/Usuarios/modelos/Usuario.ts
+++ src/app/Admin/Usuarios/modelos/Usuario.ts
@@ -1,8 +1,15 @@
+// Clase que representa a un usuario en el sistema
 export class Usuario {
+  // Propiedad que almacena el ID del usuario
+  id: number = 0;
 
-  id:number=0;
-  nombreUsuario:string=""
-  email:string=""
-  permisos:string[]=[]
+  // Propiedad que almacena el nombre de usuario
+  nombreUsuario: string = "";
 
+  // Propiedad que almacena el correo electrónico del usuario
+  email: string = "";
+
+  // Propiedad que almacena una lista de permisos asociados al usuario
+  permisos: string[] = [];
 }
+
