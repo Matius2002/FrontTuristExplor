7a1a4e76b61c3f762e2e110a5c3625b32c1e4ed6
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-22 18:01:19 -0500

    Se agregan comentarios en reporte.service.spec.ts

--- src/app/Reportes/report.service.spec.ts
+++ src/app/Reportes/report.service.spec.ts
@@ -3,14 +3,19 @@ import { TestBed } from '@angular/core/testing';
 import { ReportService } from './report.service';
 
 describe('ReportService', () => {
-  let service: ReportService;
+  let service: ReportService; // Declara una variable para el servicio a probar
 
+  // Configuración antes de cada prueba
   beforeEach(() => {
+    // Configura un módulo de prueba
     TestBed.configureTestingModule({});
+
+    // Inyecta el servicio en la variable 'service'
     service = TestBed.inject(ReportService);
   });
 
+  // Prueba que verifica que el servicio se crea correctamente
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy(); // Verifica que el servicio no sea nulo o indefinido
   });
 });
