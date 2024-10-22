32972bc1742ddf8cc7cad1e502799544d1e27c37
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-21 21:04:27 -0500

    Se agrega comentarios en login.component.spec.ts

--- src/app/Admin/Usuarios/login/login.component.spec.ts
+++ src/app/Admin/Usuarios/login/login.component.spec.ts
@@ -2,22 +2,28 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
 
+// Describe el conjunto de pruebas para el componente LoginComponent
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+  // Declara variables para el componente y el fixture
+  let component: LoginComponent; // Componente que se está probando
+  let fixture: ComponentFixture<LoginComponent>; // Fixture que permite interactuar con el componente
 
+  // Función que se ejecuta antes de cada prueba
   beforeEach(async () => {
+    // Configura el módulo de pruebas con el LoginComponent
     await TestBed.configureTestingModule({
-      imports: [LoginComponent]
+      imports: [LoginComponent] // Importa el componente que se va a probar
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compila los componentes para que estén listos para las pruebas
+
+    // Crea una instancia del componente y su fixture
     fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Asigna el componente a la variable
+    fixture.detectChanges(); // Detecta los cambios para inicializar el componente
   });
 
+  // Prueba para verificar que el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Verifica que el componente no sea nulo
   });
 });
