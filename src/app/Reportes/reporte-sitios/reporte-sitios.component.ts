34855897724c3f77e49418446e71ea2c6cac9b19
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-22 17:41:42 -0500

    Se agregan comentarios en pages.component.ts

--- src/app/Reportes/reporte-sitios/reporte-sitios.component.ts
+++ src/app/Reportes/reporte-sitios/reporte-sitios.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 
+// Decorador que marca esta clase como un componente Angular
 @Component({
-  selector: 'app-reporte-sitios',
-  standalone: true,
-  imports: [
+  selector: 'app-reporte-sitios', // El selector para usar este componente en plantillas HTML
+  standalone: true, // Indica que el componente es independiente (standalone), no necesita declararse en un módulo
+  imports: [ // Aquí se pueden importar otros componentes o módulos si fueran necesarios
 
   ],
-  templateUrl: './reporte-sitios.component.html',
-  styleUrl: './reporte-sitios.component.css'
+  templateUrl: './reporte-sitios.component.html', // Ruta al archivo HTML que contiene la plantilla de este componente
+  styleUrl: './reporte-sitios.component.css' // Ruta al archivo CSS que contiene los estilos para este componente
 })
 export class ReporteSitiosComponent {
-
+  // Clase vacía para el componente ReporteSitios
+  // Aquí iría la lógica y las propiedades del componente
 }
