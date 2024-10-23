19aa33b296851b988313f916b6b78bce99b466ac
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 16:51:27 -0500

    Se agregan comentarios en editar-noticia.component.spec.ts

--- src/app/Turismo/Noticia/editar-noticia/editar-noticia.component.spec.ts
+++ src/app/Turismo/Noticia/editar-noticia/editar-noticia.component.spec.ts
@@ -3,21 +3,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditarNoticiaComponent } from './editar-noticia.component';
 
 describe('EditarNoticiaComponent', () => {
+  // Declaramos las variables para el componente y la fixture
   let component: EditarNoticiaComponent;
   let fixture: ComponentFixture<EditarNoticiaComponent>;
 
+  // Configuramos el entorno de pruebas antes de cada prueba
   beforeEach(async () => {
+    // Configuración del TestBed para el componente
     await TestBed.configureTestingModule({
-      imports: [EditarNoticiaComponent]
+      imports: [EditarNoticiaComponent] // Importamos el componente a probar
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compilamos los componentes
+
+    // Creamos la instancia del componente y la fixture
     fixture = TestBed.createComponent(EditarNoticiaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Asignamos la instancia del componente
+    fixture.detectChanges(); // Detectamos los cambios en la fixture
   });
 
+  // Prueba para verificar que el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Comprobamos que el componente es verdadero (existe)
   });
 });
