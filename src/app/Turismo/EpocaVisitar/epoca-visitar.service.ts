d5f934e29d7744e09c81e5f1be5ea49ef65c0084
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 13:09:04 -0500

    Se agregan comentarios en epoca-visitar.service.ts

--- src/app/Turismo/EpocaVisitar/epoca-visitar.service.ts
+++ src/app/Turismo/EpocaVisitar/epoca-visitar.service.ts
@@ -3,75 +3,80 @@ import {entornos} from "../../Entorno/entornos";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {catchError, Observable, throwError} from "rxjs";
 
-interface EpocaVisitar{
-  id: number;
-  nombre: string;
-  descripcion: string;
-  clima: string;
+// Interfaz que define la estructura de un objeto EpocaVisitar
+interface EpocaVisitar {
+  id: number; // Identificador único de la época de visita
+  nombre: string; // Nombre de la época de visita
+  descripcion: string; // Descripción de la época de visita
+  clima: string; // Clima de la época de visita
 }
+
+// Decorador Injectable para que el servicio esté disponible en toda la aplicación
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Proporciona el servicio a nivel raíz
 })
 export class EpocaVisitarService {
 
   // URL BASE API
-  dynamicHost = entornos.dynamicHost;
-  private baseUrl: string = `http://${this.dynamicHost}/api`;  //Url Base API
+  dynamicHost = entornos.dynamicHost; // Obtiene el host dinámico del entorno
+  private baseUrl: string = `http://${this.dynamicHost}/api`;  // URL base para las peticiones a la API
 
-  constructor(private http: HttpClient) {
+  // Constructor del servicio que inyecta HttpClient
+  constructor(private http: HttpClient) {}
 
-  }
-  // Function para guardar un nuevo EpocaVisitar
+  // Función para guardar un nuevo EpocaVisitar
   guardarEpocaVisitar(tipoAlojamiento: EpocaVisitar): Observable<EpocaVisitar> {
     return this.http.post<EpocaVisitar>(`${this.baseUrl}/epocaVisitars/guardarEpocaVisitar`, tipoAlojamiento)
       .pipe(
-        catchError(this.handleError)
+        catchError(this.handleError) // Maneja errores en la petición
       );
   }
 
-  // Eliminar EpocaVisitar
+  // Función para eliminar un EpocaVisitar
   eliminarEpocaVisitar(id: number): Observable<void> {
     return this.http.delete<void>(`${this.baseUrl}/epocaVisitars/${id}`)
       .pipe(
-        catchError(this.handleError)
+        catchError(this.handleError) // Maneja errores en la petición
       );
   }
 
-  // Recuperar todos EpocaVisitar
+  // Función para recuperar todos los EpocaVisitar
   recuperarTodosEpocaVisitar(): Observable<EpocaVisitar[]> {
     return this.http.get<EpocaVisitar[]>(`${this.baseUrl}/epocaVisitars/obtenerTodosLosEpocaVisitar`)
       .pipe(
-        catchError(this.handleError)
-      );
-  }
-  //obtener EpocaVisitar
-  obtenerEpocaVisitar(id: number): Observable<EpocaVisitar> {
-    return this.http.get<EpocaVisitar>(`${this.baseUrl}/epocaVisitars/recuperarPorId/${id}`)
-      .pipe(
-        catchError(this.handleError)
+        catchError(this.handleError) // Maneja errores en la petición
       );
   }
 
-  // Actualizar EpocaVisitar
-  actualizarEpocaVisitar(id: number, tipoActualizada: EpocaVisitar): Observable<EpocaVisitar> {
-    // Asegúrate de que el ID en el cuerpo de la solicitud sea igual al ID en la URL
-    tipoActualizada.id = id;
-    return this.http.put<EpocaVisitar>(`${this.baseUrl}/epocaVisitars/${id}`, tipoActualizada)
+  // Función para obtener un EpocaVisitar por su ID
+  obtenerEpocaVisitar(id: number): Observable<EpocaVisitar> {
+    return this.http.get<EpocaVisitar>(`${this.baseUrl}/epocaVisitars/recuperarPorId/${id}`)
       .pipe(
-        catchError(this.handleError)
+        catchError(this.handleError) // Maneja errores en la petición
       );
   }
-  // verificar EpocaVisitar ya existe en la base de datos
+
+  // Función para actualizar un EpocaVisitar
+  actualizarEpocaVisitar(id: number, tipoActualizada: EpocaVisitar): Observable<EpocaVisitar> {
+    // Asegúrate de que el ID en el cuerpo de la solicitud sea igual al ID en la URL
+    tipoActualizada.id = id; // Asigna el ID al objeto actualizado
+    return this.http.put<EpocaVisitar>(`${this.baseUrl}/epocaVisitars/${id}`, tipoActualizada)
+      .pipe(
+        catchError(this.handleError) // Maneja errores en la petición
+      );
+  }
+
+  // Función para verificar si una EpocaVisitar ya existe en la base de datos
   verificarEpocaVisitarExistente(nombre: string): Observable<boolean> {
     return this.http.get<boolean>(`${this.baseUrl}/epocaVisitars/existe/${encodeURIComponent(nombre)}`)
       .pipe(
-        catchError(this.handleError)
+        catchError(this.handleError) // Maneja errores en la petición
       );
   }
 
   // Función para manejar errores de HTTP
   private handleError(error: HttpErrorResponse): Observable<any> {
-    let errorMessage = 'Error desconocido';
+    let errorMessage = 'Error desconocido'; // Mensaje de error por defecto
     if (error.error instanceof ErrorEvent) {
       // Error del lado del cliente
       errorMessage = `Error: ${error.error.message}`;
@@ -79,7 +84,7 @@ export class EpocaVisitarService {
       // Error del lado del servidor
       errorMessage = `Código de error: ${error.status}, mensaje: ${error.error.message}`;
     }
-    console.error(errorMessage);
-    return throwError(errorMessage);
+    console.error(errorMessage); // Imprime el error en la consola
+    return throwError(errorMessage); // Lanza el error
   }
 }
