7f16f082673cf5b299072a53e9d4f11d2fd2b272
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 11:44:30 -0500

    Se agregan comentarios en nuevo-alojamiento.component.spec.ts

--- src/app/Turismo/Alojamiento/nuevo-alojamiento/nuevo-alojamiento.component.spec.ts
+++ src/app/Turismo/Alojamiento/nuevo-alojamiento/nuevo-alojamiento.component.spec.ts
@@ -3,21 +3,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NuevoAlojamientoComponent } from './nuevo-alojamiento.component';
 
 describe('NuevoAlojamientoComponent', () => {
-  let component: NuevoAlojamientoComponent;
-  let fixture: ComponentFixture<NuevoAlojamientoComponent>;
+  let component: NuevoAlojamientoComponent; // Variable para almacenar la instancia del componente
+  let fixture: ComponentFixture<NuevoAlojamientoComponent>; // Variable para la instancia del fixture
 
+  // Configuración del módulo de prueba antes de cada prueba
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NuevoAlojamientoComponent]
+      imports: [NuevoAlojamientoComponent] // Importa el componente que se va a probar
     })
-    .compileComponents();
-    
-    fixture = TestBed.createComponent(NuevoAlojamientoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+      .compileComponents(); // Compila los componentes
   });
 
+  // Configuración del fixture y la instancia del componente antes de cada prueba
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NuevoAlojamientoComponent); // Crea una instancia del componente
+    component = fixture.componentInstance; // Asigna la instancia del componente a la variable
+    fixture.detectChanges(); // Detecta cambios para inicializar el componente
+  });
+
+  // Prueba para verificar que el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Verifica que la instancia del componente sea verdadera
   });
 });
