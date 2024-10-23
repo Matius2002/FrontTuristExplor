1dcb2e1930bc7282f3571e630be61d469823ce43
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 16:12:51 -0500

    Se agregan comentarios en experiencia-contenido.component.ts

--- src/app/Turismo/Experiencia/experiencia-contenido/experiencia-contenido.component.ts
+++ src/app/Turismo/Experiencia/experiencia-contenido/experiencia-contenido.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
 
+// Decorador @Component define las características principales del componente
 @Component({
+  // 'app-experiencia-contenido' será el nombre del selector que se usará en el HTML para insertar este componente
   selector: 'app-experiencia-contenido',
+
+  // standalone: true indica que este componente no forma parte de un módulo, es autónomo
   standalone: true,
+
+  // imports define qué otros módulos o componentes se importan para usar en este componente (aquí está vacío)
   imports: [],
+
+  // templateUrl hace referencia a la ruta del archivo HTML asociado al componente
   templateUrl: './experiencia-contenido.component.html',
+
+  // styleUrl hace referencia a la ruta del archivo CSS asociado al componente
   styleUrl: './experiencia-contenido.component.css'
 })
-export class ExperienciaContenidoComponent {
 
+// Definición de la clase del componente 'ExperienciaContenidoComponent'
+export class ExperienciaContenidoComponent {
+  // Actualmente, la clase está vacía, pero aquí es donde iría la lógica del componente
 }
+
