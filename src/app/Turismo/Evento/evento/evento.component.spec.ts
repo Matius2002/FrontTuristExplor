fcebeba4a94b275816ccf4235f9d7b1d793ee372
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 13:21:04 -0500

    Se agregan comentarios en eventos.component.spec.ts

--- src/app/Turismo/Evento/evento/evento.component.spec.ts
+++ src/app/Turismo/Evento/evento/evento.component.spec.ts
@@ -2,22 +2,28 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EventoComponent } from './evento.component';
 
+// Describe el conjunto de pruebas para el EventoComponent
 describe('EventoComponent', () => {
+  // Declara las variables para el componente y su fixture
   let component: EventoComponent;
   let fixture: ComponentFixture<EventoComponent>;
 
+  // Antes de ejecutar las pruebas, configura el entorno de pruebas
   beforeEach(async () => {
+    // Configura el módulo de pruebas para incluir el EventoComponent
     await TestBed.configureTestingModule({
-      imports: [EventoComponent]
+      imports: [EventoComponent] // Importa el componente que se va a probar
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compila los componentes del módulo
+
+    // Crea una instancia del componente y de su fixture
     fixture = TestBed.createComponent(EventoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Asigna la instancia del componente
+    fixture.detectChanges(); // Detecta los cambios para inicializar el componente
   });
 
+  // Test para verificar que el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Comprueba que la instancia del componente es verdadera (existe)
   });
 });
