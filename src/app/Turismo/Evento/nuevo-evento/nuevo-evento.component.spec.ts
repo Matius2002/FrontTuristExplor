e5edb09ea982deb1144d5ff86b11f45a0e2ad5ac
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 13:33:45 -0500

    Se agregan comentarios en evento-contenido.component.spec.ts

--- src/app/Turismo/Evento/nuevo-evento/nuevo-evento.component.spec.ts
+++ src/app/Turismo/Evento/nuevo-evento/nuevo-evento.component.spec.ts
@@ -3,21 +3,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NuevoEventoComponent } from './nuevo-evento.component';
 
 describe('NuevoEventoComponent', () => {
-  let component: NuevoEventoComponent;
-  let fixture: ComponentFixture<NuevoEventoComponent>;
+  let component: NuevoEventoComponent; // Declara una variable para el componente
+  let fixture: ComponentFixture<NuevoEventoComponent>; // Declara una variable para la instancia del componente
 
+  // Configura el entorno de pruebas antes de cada prueba
   beforeEach(async () => {
+    // Configura el módulo de pruebas
     await TestBed.configureTestingModule({
-      imports: [NuevoEventoComponent]
+      imports: [NuevoEventoComponent] // Importa el componente a probar
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compila los componentes asíncronamente
+
+    // Crea una instancia del componente y su fixture
     fixture = TestBed.createComponent(NuevoEventoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Obtiene la instancia del componente
+    fixture.detectChanges(); // Detecta cambios en la vista del componente
   });
 
+  // Prueba para verificar que el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Asegura que la instancia del componente sea verdadera
   });
 });
