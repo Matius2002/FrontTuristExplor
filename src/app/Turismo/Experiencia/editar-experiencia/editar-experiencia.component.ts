4f3c73f58726cd5fdbbddc0522cac127b37e6595
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 16:03:32 -0500

    Se agregan comentarios en editar-experiencia.component.ts

--- src/app/Turismo/Experiencia/editar-experiencia/editar-experiencia.component.ts
+++ src/app/Turismo/Experiencia/editar-experiencia/editar-experiencia.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 
+// Decorador @Component define un componente de Angular
 @Component({
+  // El selector define cómo se llamará este componente en el HTML
   selector: 'app-editar-experiencia',
-  standalone: true,
-  imports: [],
-  templateUrl: './editar-experiencia.component.html',
-  styleUrl: './editar-experiencia.component.css'
-})
-export class EditarExperienciaComponent {
 
+  // 'standalone: true' indica que este es un componente autónomo que no requiere un módulo para su uso
+  standalone: true,
+
+  // imports es donde puedes especificar otros módulos o componentes que se necesitan para este componente
+  imports: [],
+
+  // templateUrl apunta al archivo HTML que contiene el template del componente
+  templateUrl: './editar-experiencia.component.html',
+
+  // styleUrls apunta al archivo CSS que contiene los estilos específicos para este componente
+  styleUrls: ['./editar-experiencia.component.css']
+})
+// Clase que define la lógica del componente EditarExperienciaComponent
+export class EditarExperienciaComponent {
 }
