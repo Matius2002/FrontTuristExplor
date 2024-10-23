5acaa7a69ef4e8ee81047120f872466d281b6e9f
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 16:02:11 -0500

    Se agregan comentarios en editar-experiencia.component.spec.ts

--- src/app/Turismo/Experiencia/editar-experiencia/editar-experiencia.component.spec.ts
+++ src/app/Turismo/Experiencia/editar-experiencia/editar-experiencia.component.spec.ts
@@ -2,22 +2,29 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditarExperienciaComponent } from './editar-experiencia.component';
 
+// Descripción del bloque de pruebas para el componente EditarExperienciaComponent
 describe('EditarExperienciaComponent', () => {
+
+  // Variables que se utilizarán para almacenar el componente y su fixture (el contenedor para el componente)
   let component: EditarExperienciaComponent;
   let fixture: ComponentFixture<EditarExperienciaComponent>;
 
+  // Bloque que se ejecuta antes de cada prueba, configurando el entorno de pruebas
   beforeEach(async () => {
+    // Configura el módulo de pruebas con el componente EditarExperienciaComponent
     await TestBed.configureTestingModule({
-      imports: [EditarExperienciaComponent]
+      imports: [EditarExperienciaComponent] // Importa el componente que se está probando
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compila los componentes y sus dependencias
+
+    // Crea una instancia de fixture y componente para interactuar con ellos
     fixture = TestBed.createComponent(EditarExperienciaComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Obtiene la instancia del componente desde el fixture
+    fixture.detectChanges(); // Detecta los cambios y actualiza la vista del componente
   });
 
+  // Prueba para verificar que el componente se haya creado correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Verifica que el componente exista (que haya sido creado correctamente)
   });
 });
