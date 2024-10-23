07f115e116f25625965faef430896981952e9358
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 13:35:23 -0500

    Se agregan comentarios en evento.service.spec.ts

--- src/app/Turismo/Evento/evento.service.spec.ts
+++ src/app/Turismo/Evento/evento.service.spec.ts
@@ -3,14 +3,16 @@ import { TestBed } from '@angular/core/testing';
 import { EventoService } from './evento.service';
 
 describe('EventoService', () => {
-  let service: EventoService;
+  let service: EventoService; // Declara una variable para el servicio
 
+  // Configura el entorno de pruebas antes de cada prueba
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EventoService);
+    TestBed.configureTestingModule({}); // Configura el módulo de pruebas sin importar módulos adicionales
+    service = TestBed.inject(EventoService); // Inyecta la instancia del servicio en la variable 'service'
   });
 
+  // Prueba para verificar que el servicio se crea correctamente
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy(); // Asegura que la instancia del servicio sea verdadera
   });
 });
