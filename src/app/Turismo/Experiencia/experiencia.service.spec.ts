ff378ce5616292f034d4a5eaa78cc8ed7143328e
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 16:14:23 -0500

    Se agregan comentarios en experiencia-contenido.service.spec.ts

--- src/app/Turismo/Experiencia/experiencia.service.spec.ts
+++ src/app/Turismo/Experiencia/experiencia.service.spec.ts
@@ -2,15 +2,24 @@ import { TestBed } from '@angular/core/testing';
 
 import { ExperienciaService } from './experiencia.service';
 
+// Definición del bloque de pruebas para el servicio 'ExperienciaService'
 describe('ExperienciaService', () => {
+
+  // Declaramos una variable para almacenar la instancia del servicio
   let service: ExperienciaService;
 
+  // beforeEach se ejecuta antes de cada prueba para configurar el entorno de prueba
   beforeEach(() => {
+    // Configuración del entorno de pruebas para el servicio 'ExperienciaService'
     TestBed.configureTestingModule({});
+
+    // Inyección del servicio 'ExperienciaService' en la variable 'service' mediante TestBed
     service = TestBed.inject(ExperienciaService);
   });
 
+  // Prueba básica que verifica si el servicio fue creado correctamente
   it('should be created', () => {
+    // La expectativa es que el servicio exista (sea truthy)
     expect(service).toBeTruthy();
   });
 });
