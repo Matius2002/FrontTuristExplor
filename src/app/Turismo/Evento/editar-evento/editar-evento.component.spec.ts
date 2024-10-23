5148e76e993e6c5ea6a047542512d6083cec23c3
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 13:13:16 -0500

    Se agregan comentarios en editar-evento.component.spec.ts

--- src/app/Turismo/Evento/editar-evento/editar-evento.component.spec.ts
+++ src/app/Turismo/Evento/editar-evento/editar-evento.component.spec.ts
@@ -2,22 +2,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { EditarEventoComponent } from './editar-evento.component';
 
+// Descripción del bloque de pruebas para el componente EditarEventoComponent
 describe('EditarEventoComponent', () => {
-  let component: EditarEventoComponent;
-  let fixture: ComponentFixture<EditarEventoComponent>;
+  let component: EditarEventoComponent; // Declaración de la variable que contendrá la instancia del componente
+  let fixture: ComponentFixture<EditarEventoComponent>; // Declaración de la variable que contendrá la configuración de la prueba del componente
 
+  // Función que se ejecuta antes de cada prueba
   beforeEach(async () => {
+    // Configuración del módulo de prueba
     await TestBed.configureTestingModule({
-      imports: [EditarEventoComponent]
+      imports: [EditarEventoComponent] // Importa el componente a probar
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compila los componentes y plantillas
+
+    // Crea una instancia del componente y su fixture
     fixture = TestBed.createComponent(EditarEventoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Obtiene la instancia del componente
+    fixture.detectChanges(); // Detecta cambios para inicializar la vista
   });
 
+  // Prueba para verificar que el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Afirmación para verificar que el componente fue creado
   });
 });
