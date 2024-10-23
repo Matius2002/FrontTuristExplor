af17d91ed6b89c848bceec519948653f61180442
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 12:19:34 -0500

    Se agregan comentarios en atraciones-principal.component.ts

--- src/app/Turismo/AtracionPrincipal/atraciones-principal/atraciones-principal.component.ts
+++ src/app/Turismo/AtracionPrincipal/atraciones-principal/atraciones-principal.component.ts
@@ -14,82 +14,90 @@ import {Router} from "@angular/router";
 import Swal from "sweetalert2";
 import {EditarAtracionesComponent} from "../editar-atraciones/editar-atraciones.component";
 import * as ExcelJS from "exceljs";
-interface Atraciones{
-  id: number;
-  nombre: string;
-  descripcion: string;
-  horarioFuncionamiento: string;
-  horarioFin: string;
+
+// Definición de la interfaz Atraciones
+interface Atraciones {
+  id: number; // Identificador único de la atracción
+  nombre: string; // Nombre de la atracción
+  descripcion: string; // Descripción de la atracción
+  horarioFuncionamiento: string; // Horario de funcionamiento de la atracción
+  horarioFin: string; // Horario de cierre de la atracción
 }
+
+// Definición de la interfaz Item
 interface Item {
-  id: number;
-  nombre: string;
+  id: number; // Identificador único del ítem
+  nombre: string; // Nombre del ítem
 }
+
 @Component({
-  providers: [AtracionesService, HttpClient],
-  selector: 'app-atraciones-principal',
-  standalone: true,
+  providers: [AtracionesService, HttpClient], // Servicios proporcionados en este componente
+  selector: 'app-atraciones-principal', // Selector del componente
+  standalone: true, // Indica que este componente puede funcionar por sí mismo
   imports: [
-    FilterPipe,
-    HttpClientModule,
-    NgIf,
-    DecimalPipe,
-    CommonModule,
-    MatInputModule,
-    MatFormFieldModule,
-    MatCell,
-    MatCellDef,
-    FormsModule,
-    TooltipDirective,
-    NgxPaginationModule,
-    FilterPipe,
+    FilterPipe, // Pipe para filtrar datos
+    HttpClientModule, // Módulo para realizar solicitudes HTTP
+    NgIf, // Directiva para mostrar/ocultar elementos
+    DecimalPipe, // Pipe para formatear números decimales
+    CommonModule, // Módulo común de Angular
+    MatInputModule, // Módulo de Angular Material para inputs
+    MatFormFieldModule, // Módulo de Angular Material para campos de formulario
+    MatCell, // Clase para celdas de tablas de Angular Material
+    MatCellDef, // Clase para definir celdas de tablas de Angular Material
+    FormsModule, // Módulo para formularios en Angular
+    TooltipDirective, // Directiva para mostrar tooltips
+    NgxPaginationModule, // Módulo para paginación
+    FilterPipe, // Pipe para filtrar datos
   ],
-  templateUrl: './atraciones-principal.component.html',
-  styleUrl: './atraciones-principal.component.css'
+  templateUrl: './atraciones-principal.component.html', // Ruta de la plantilla HTML
+  styleUrls: ['./atraciones-principal.component.css'] // Ruta de los estilos CSS
 })
-export class AtracionesPrincipalComponent implements  OnInit{
-  isHelpModalVisible: boolean = false;
-  atraciones: Atraciones[] = [];
+export class AtracionesPrincipalComponent implements OnInit { // Clase del componente
+  isHelpModalVisible: boolean = false; // Indica si el modal de ayuda está visible
+  atraciones: Atraciones[] = []; // Array para almacenar las atracciones
   page = 1; // Inicializa la página en 1
   itemsPerPage = 5; // Número de elementos por página
   totalPages = 0; // Número total de páginas
   currentColumn: string = 'id'; // Columna inicial para ordenar
-  sortOrder: string = 'asc';
+  sortOrder: string = 'asc'; // Orden de clasificación
 
-  // Eliminar
-  showAlert: boolean = false;
-  atracionesToDelete: Atraciones | null = null;
-  atracionesEliminado: boolean = false;
-  errorMessage: string | null = null;
+  // Propiedades para manejo de eliminación
+  showAlert: boolean = false; // Indica si se debe mostrar una alerta
+  atracionesToDelete: Atraciones | null = null; // Atracción seleccionada para eliminar
+  atracionesEliminado: boolean = false; // Indica si la atracción ha sido eliminada
+  errorMessage: string | null = null; // Mensaje de error, si ocurre
 
-  //búsqueda
-  searchText: string = '';
-  items: Item[] = [];
-  filteredItems: Item[] = [];
-  displayedItems: Item[] = [];
-  searchNotFound: boolean = false;
+  // Propiedades para búsqueda
+  searchText: string = ''; // Texto de búsqueda
+  items: Item[] = []; // Array para almacenar los ítems
+  filteredItems: Item[] = []; // Ítems filtrados según búsqueda
+  displayedItems: Item[] = []; // Ítems que se mostrarán
+  searchNotFound: boolean = false; // Indica si no se encontraron resultados
 
   constructor(
-    private atracionesService: AtracionesService,
-    private dialog: MatDialog,
-    private router: Router,
-  ) {
-  }
+    private atracionesService: AtracionesService, // Servicio para manejar atracciones
+    private dialog: MatDialog, // Servicio para manejar diálogos
+    private router: Router, // Servicio para la navegación
+  ) { }
+
   ngOnInit(): void {
-    this.cargarAtraciones();
+    this.cargarAtraciones(); // Carga las atracciones al inicializar el componente
   }
-  //Imprimir
+
+  // Metodo para imprimir la tabla de atracciones
   printTable() {
-    window.print();
+    window.print(); // Imprime la ventana actual
   }
-// Actualizar atraccion
+
+  // Metodo para abrir el modal de actualización de una atracción
   openUpdateModal(atraccion: Atraciones): void {
     const dialogRef = this.dialog.open(EditarAtracionesComponent, {
-      width: '400px',
-      data: { atraciones: atraccion }
+      width: '400px', // Ancho del modal
+      data: { atraciones: atraccion } // Datos pasados al modal
     });
 
     dialogRef.afterClosed().subscribe(result => {
+      // Manejo del resultado al cerrar el modal
       if (result === 'success') {
         Swal.fire('¡Actualizado!', 'La Atracción se ha actualizado correctamente.', 'success');
       } else if (result === 'error') {
@@ -97,13 +105,16 @@ export class AtracionesPrincipalComponent implements  OnInit{
       }
     });
   }
-  protected readonly Math = Math;
+
+  protected readonly Math = Math; // Proporciona acceso a la clase Math
+
+  // Metodo para cargar las atracciones desde el servicio
   cargarAtraciones() {
-    // Lógica para cargar los datos de la base de datos.
     this.atracionesService.recuperarTodosAtraciones().subscribe(
       data => {
         console.log("Datos recibidos del servidor:", data);
         console.log("Cantidad de registros recibidos:", data.length);
+        // Mapea los datos recibidos a la estructura de Atraciones
         this.atraciones = data.map(atraccion => {
           return {
             id: atraccion.id,
@@ -113,44 +124,52 @@ export class AtracionesPrincipalComponent implements  OnInit{
             horarioFin: atraccion.horarioFin,
           };
         });
+        // Calcula el número total de páginas
         this.totalPages = Math.ceil(this.atraciones.length / this.itemsPerPage);
-
         console.log("Datos de los tipos de Alojamiento cargados correctamente:", this.atraciones);
       },
       error => {
-        console.error('Error al cargar los tipos de Alojamiento:', error);
+        console.error('Error al cargar los tipos de Alojamiento:', error); // Manejo de errores
       }
     );
   }
-  exportToExcel() {
-    const workbook = new ExcelJS.Workbook();
-    const worksheet = workbook.addWorksheet('TipoAlojamiento');
-    worksheet.columns = [
-      {header: 'ID', key: 'id', width: 10},
-      {header: 'Nombre', key: 'nombre', width: 30},
-      {header: 'Descripción', key: 'descripcion', width: 15},
-      {header: 'horario Funcionamiento', key: 'horarioFuncionamiento', width: 15},
-      {header: 'horario Fin', key: 'horarioFin', width: 15},
 
+  // Metodo para exportar las atracciones a un archivo Excel
+  exportToExcel() {
+    const workbook = new ExcelJS.Workbook(); // Crea un nuevo libro de trabajo
+    const worksheet = workbook.addWorksheet('TipoAlojamiento'); // Agrega una nueva hoja
+    worksheet.columns = [
+      { header: 'ID', key: 'id', width: 10 }, // Columna ID
+      { header: 'Nombre', key: 'nombre', width: 30 }, // Columna Nombre
+      { header: 'Descripción', key: 'descripcion', width: 15 }, // Columna Descripción
+      { header: 'horario Funcionamiento', key: 'horarioFuncionamiento', width: 15 }, // Columna Horario de Funcionamiento
+      { header: 'horario Fin', key: 'horarioFin', width: 15 }, // Columna Horario de Fin
     ];
+    // Agrega cada atracción a la hoja
     this.atraciones.forEach(atraccion => {
       worksheet.addRow(atraccion);
     });
+    // Escribe el buffer de Excel y crea un blob para descargar
     workbook.xlsx.writeBuffer().then(data => {
-      const blob = new Blob([data], {type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'});
-      const url = window.URL.createObjectURL(blob);
-      window.open(url);
+      const blob = new Blob([data], { type: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' });
+      const url = window.URL.createObjectURL(blob); // Crea un objeto URL para el blob
+      window.open(url); // Abre la URL en una nueva pestaña
     });
   }
+
+  // Metodo para ordenar las columnas de la tabla
   sortColumn(columnName: string) {
     if (this.currentColumn === columnName) {
+      // Cambia el orden si se vuelve a hacer clic en la misma columna
       this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
     } else {
+      // Cambia a la nueva columna y establece el orden ascendente
       this.currentColumn = columnName;
       this.sortOrder = 'asc';
     }
-    this.sortData();
+    this.sortData(); // Ordena los datos según la configuración
   }
+
   sortData() {
     if (this.currentColumn) {
       this.atraciones.sort((a, b) => {
