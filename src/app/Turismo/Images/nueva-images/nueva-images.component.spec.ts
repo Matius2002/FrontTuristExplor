33915659ee5909487d5d22c838144c928476dfd9
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 16:35:40 -0500

    Se agregan comentarios en nueva-imagenes.component.spec.ts

--- src/app/Turismo/Images/nueva-images/nueva-images.component.spec.ts
+++ src/app/Turismo/Images/nueva-images/nueva-images.component.spec.ts
@@ -3,21 +3,25 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NuevaImagesComponent } from './nueva-images.component';
 
 describe('NuevaImagesComponent', () => {
-  let component: NuevaImagesComponent;
-  let fixture: ComponentFixture<NuevaImagesComponent>;
+  let component: NuevaImagesComponent; // Declaración de la variable para el componente
+  let fixture: ComponentFixture<NuevaImagesComponent>; // Declaración de la variable para el fixture del componente
 
+  // Bloque que se ejecuta antes de cada prueba
   beforeEach(async () => {
+    // Configuración del módulo de pruebas
     await TestBed.configureTestingModule({
-      imports: [NuevaImagesComponent]
+      imports: [NuevaImagesComponent] // Importa el componente que se está probando
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compila los componentes para su uso en las pruebas
+
+    // Crea una instancia del componente y del fixture
     fixture = TestBed.createComponent(NuevaImagesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Obtiene la instancia del componente
+    fixture.detectChanges(); // Detecta cambios para inicializar el componente
   });
 
+  // Prueba para verificar si el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Afirmación que verifica que el componente existe
   });
 });
