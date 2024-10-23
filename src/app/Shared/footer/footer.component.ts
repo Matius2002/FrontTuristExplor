62edbeec705541da73d396acfe013e05c9d2fb45
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-22 19:23:02 -0500

    Se agregan comentarios en footer.component.ts

--- src/app/Shared/footer/footer.component.ts
+++ src/app/Shared/footer/footer.component.ts
@@ -5,8 +5,8 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css' // Corrige 'styleUrl' a 'styleUrls'
 })
 export class FooterComponent {
-
+  // Aquí se pueden agregar propiedades o métodos relacionados con el footer
 }
