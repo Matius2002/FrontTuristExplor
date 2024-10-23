699389f29d2e99fb09a495b2f74412dba9d875a4
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 11:58:26 -0500

    Se agregan comentarios en nuevo-alojamiento.component.ts

--- src/app/Turismo/Alojamiento/nuevo-alojamiento/nuevo-alojamiento.component.ts
+++ src/app/Turismo/Alojamiento/nuevo-alojamiento/nuevo-alojamiento.component.ts
@@ -6,129 +6,148 @@ import {AlojamientoService} from "../alojamiento.service";
 import {Router} from "@angular/router";
 import {catchError, of} from "rxjs";
 import Swal from "sweetalert2";
-interface Destinos {
-  id: number;
-  destinoName: string;
-}
-interface  TipoAlojamiento{
-  id: number;
-  nombre: string;
-}
-interface Images {
-  id: number;
-  nombre: string;
-  ruta: string;
-  activa: boolean;
-}
-interface Alojamiento{
-  id: number;
-  descripcion: string;
-  destinos: Destinos [];
-  nombre: string;
-  tipoAlojamiento: TipoAlojamiento;
-  direccion: string;
-  celular: string;
-  email: string;
-  webUrl: string;
-  precioGeneral: number;
-  fechaCreacion: Date;
-  imagenes: Images[];
-}
-@Component({
-  providers: [AlojamientoService, HttpClient],
-  selector: 'app-nuevo-alojamiento',
-  standalone: true,
-  imports: [
-    HttpClientModule,
-    FormsModule,
-    NgIf,
-    ReactiveFormsModule,
-    NgForOf,
 
+// Interfaz para representar un destino
+interface Destinos {
+  id: number; // Identificador único del destino
+  destinoName: string; // Nombre del destino
+}
+
+// Interfaz para representar el tipo de alojamiento
+interface TipoAlojamiento {
+  id: number; // Identificador único del tipo de alojamiento
+  nombre: string; // Nombre del tipo de alojamiento
+}
+
+// Interfaz para representar una imagen
+interface Images {
+  id: number; // Identificador único de la imagen
+  nombre: string; // Nombre de la imagen
+  ruta: string; // Ruta donde se encuentra la imagen
+  activa: boolean; // Indica si la imagen está activa
+}
+
+// Interfaz para representar un alojamiento
+interface Alojamiento {
+  id: number; // Identificador único del alojamiento
+  descripcion: string; // Descripción del alojamiento
+  destinos: Destinos[]; // Array de destinos asociados al alojamiento
+  nombre: string; // Nombre del alojamiento
+  tipoAlojamiento: TipoAlojamiento; // Tipo de alojamiento asociado
+  direccion: string; // Dirección del alojamiento
+  celular: string; // Número de celular de contacto
+  email: string; // Correo electrónico de contacto
+  webUrl: string; // URL del sitio web del alojamiento
+  precioGeneral: number; // Precio general del alojamiento
+  fechaCreacion: Date; // Fecha de creación del alojamiento
+  imagenes: Images[]; // Array de imágenes asociadas al alojamiento
+}
+
+@Component({
+  providers: [AlojamientoService, HttpClient], // Servicios utilizados en el componente
+  selector: 'app-nuevo-alojamiento', // Selector del componente
+  standalone: true, // Indica que el componente es autónomo
+  imports: [
+    HttpClientModule, // Módulo para hacer peticiones HTTP
+    FormsModule, // Módulo para manejar formularios
+    NgIf, // Directiva para condiciones en el HTML
+    ReactiveFormsModule, // Módulo para formularios reactivos
+    NgForOf, // Directiva para iterar sobre colecciones en el HTML
   ],
-  templateUrl: './nuevo-alojamiento.component.html',
-  styleUrl: './nuevo-alojamiento.component.css'
+  templateUrl: './nuevo-alojamiento.component.html', // Ruta al archivo de plantilla HTML
+  styleUrls: ['./nuevo-alojamiento.component.css'] // Ruta al archivo de estilos CSS
 })
-export class NuevoAlojamientoComponent implements OnInit{
-  isSubmitting: boolean = false;
-  crearForm!: FormGroup;
-  alojamientos!: Alojamiento;
-  tipoAlojamientos: TipoAlojamiento []=[];
-  destinos: Destinos[] = [];
-  imagenes: Images []=[];
+export class NuevoAlojamientoComponent implements OnInit {
+  isSubmitting: boolean = false; // Indica si se está enviando el formulario
+  crearForm!: FormGroup; // Grupo de formularios para manejar los datos del nuevo alojamiento
+  alojamientos!: Alojamiento; // Objeto para almacenar los datos del alojamiento
+  tipoAlojamientos: TipoAlojamiento[] = []; // Array para almacenar los tipos de alojamiento
+  destinos: Destinos[] = []; // Array para almacenar los destinos
+  imagenes: Images[] = []; // Array para almacenar las imágenes
 
   constructor(
-    private formBuilder: FormBuilder,
-    //public dialogRef: MatDialogRef<NuevoAlojamientoComponent>,
-    //@Inject(MAT_DIALOG_DATA) public data: any,
-    private alojamientoService: AlojamientoService,
-    private router: Router,
+    private formBuilder: FormBuilder, // Constructor del formulario reactivo
+    //public dialogRef: MatDialogRef<NuevoAlojamientoComponent>, // Referencia al diálogo (opcional)
+    //@Inject(MAT_DIALOG_DATA) public data: any, // Datos inyectados en el diálogo (opcional)
+    private alojamientoService: AlojamientoService, // Servicio para manejar alojamientos
+    private router: Router, // Servicio para manejar la navegación
   ) {}
+
   ngOnInit(): void {
+    // Inicializa el componente
     this.crearForm = this.formBuilder.group({
+      // Define el grupo de formularios con sus respectivos validadores
       nombre: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(50)]],
       descripcion: ['', [Validators.required, Validators.minLength(10), Validators.maxLength(250)]],
       tipoAlojamiento: ['', [Validators.required]],
-      destinos: [[], [Validators.required]],
+      destinos: [[], [Validators.required]], // Inicializa como array vacío
       direccion: ['', [Validators.required]],
       celular: ['', [Validators.required]],
       email: ['', [Validators.required]],
       webUrl: ['', [Validators.required]],
       precioGeneral: ['', [Validators.required]],
-      fechaCreacion: ['', [Validators.required]],
-      imagenes: [[], [Validators.required]],
+      fechaCreacion: ['', [Validators.required]], // Campo para la fecha de creación
+      imagenes: [[], [Validators.required]], // Inicializa como array vacío
     });
+    // Llama a métodos para cargar datos necesarios
     this.cargarTiposAlojamientos();
     this.cargarDestinos();
     this.cargarImages();
   }
 
+  // Método para cargar las imágenes desde el servicio
   cargarImages(): void {
     this.alojamientoService.recuperarTodosImages().subscribe(
       (imagenes: Images[]) => {
-        this.imagenes = imagenes;
-        console.log('Imágenes cargadas:', this.imagenes);
+        this.imagenes = imagenes; // Asigna las imágenes cargadas al array
+        console.log('Imágenes cargadas:', this.imagenes); // Log para verificar las imágenes cargadas
       },
       (error) => {
-        console.error('Error al cargar imágenes:', error);
+        console.error('Error al cargar imágenes:', error); // Manejo de errores en la carga de imágenes
       }
     );
   }
 
-  cargarTiposAlojamientos(): void{
+  // Método para cargar los tipos de alojamiento desde el servicio
+  cargarTiposAlojamientos(): void {
     this.alojamientoService.recuperarTodosTiposAlojamiento().subscribe(
-      (tipos: TipoAlojamiento[]) =>{
-        this.tipoAlojamientos = tipos;
+      (tipos: TipoAlojamiento[]) => {
+        this.tipoAlojamientos = tipos; // Asigna los tipos de alojamiento cargados al array
       },
       (error) => {
-        console.error('error al cargar el tipo de alojamiento',error);
+        console.error('Error al cargar el tipo de alojamiento', error); // Manejo de errores en la carga de tipos de alojamiento
       }
     );
   }
-  cargarDestinos(): void{
-    this.alojamientoService.recuperarTodosDestinos().subscribe(
-      (destinos: Destinos[]) =>{
-        this.destinos = destinos;
-      },
-      (error) => {
-        console.error(error);
-      }
-    );
-  }
-  onSubmit(): void {
-    if (this.crearForm.valid) {
-      const nombre = this.crearForm.get('nombre')?.value;
-      const descripcion = this.crearForm.get('descripcion')?.value;
-      const direccion = this.crearForm.get('direccion')?.value;
-      const celular = this.crearForm.get('celular')?.value;
-      const email = this.crearForm.get('email')?.value;
-      const webUrl = this.crearForm.get('webUrl')?.value;
-      const precioGeneral = this.crearForm.get('precioGeneral')?.value;
-      const fechaCreacion = this.crearForm.get('fechaCreacion')?.value;
-      const destinos = this.crearForm.get('destinos')?.value; // Si tienes un control para destinos
-      const tipoAlojamiento = this.crearForm.get('tipoAlojamiento')?.value; // Si tienes un control para tipoAlojamiento
-      const images = this.crearForm.get('imagenes')?.value;
 
+  // Método para cargar los destinos desde el servicio
+  cargarDestinos(): void {
+    this.alojamientoService.recuperarTodosDestinos().subscribe(
+      (destinos: Destinos[]) => {
+        this.destinos = destinos; // Asigna los destinos cargados al array
+      },
+      (error) => {
+        console.error(error); // Manejo de errores en la carga de destinos
+      }
+    );
+  }
+
+  // Método que se ejecuta al enviar el formulario
+  onSubmit(): void {
+    if (this.crearForm.valid) { // Verifica si el formulario es válido
+      const nombre = this.crearForm.get('nombre')?.value; // Obtiene el valor del campo nombre
+      const descripcion = this.crearForm.get('descripcion')?.value; // Obtiene el valor del campo descripción
+      const direccion = this.crearForm.get('direccion')?.value; // Obtiene el valor del campo dirección
+      const celular = this.crearForm.get('celular')?.value; // Obtiene el valor del campo celular
+      const email = this.crearForm.get('email')?.value; // Obtiene el valor del campo email
+      const webUrl = this.crearForm.get('webUrl')?.value; // Obtiene el valor del campo URL del sitio web
+      const precioGeneral = this.crearForm.get('precioGeneral')?.value; // Obtiene el valor del campo precio general
+      const fechaCreacion = this.crearForm.get('fechaCreacion')?.value; // Obtiene el valor del campo fecha de creación
+      const destinos = this.crearForm.get('destinos')?.value; // Obtiene el valor del campo destinos
+      const tipoAlojamiento = this.crearForm.get('tipoAlojamiento')?.value; // Obtiene el valor del campo tipo de alojamiento
+      const images = this.crearForm.get('imagenes')?.value; // Obtiene el valor del campo imágenes
+
+      // Log para verificar los datos a enviar al backend
       console.log('Datos a enviar al backend:');
       console.log('nombre:', nombre);
       console.log('descripcion:', descripcion);
@@ -142,8 +161,9 @@ export class NuevoAlojamientoComponent implements OnInit{
       console.log('tipoAlojamiento:', tipoAlojamiento);
       console.log('imagenes:', images);
 
+      // Crea un objeto de alojamiento con los datos del formulario
       const alojamientoData: Alojamiento = {
-        id: 0,
+        id: 0, // Asigna 0 como ID inicial (se generará en el backend)
         nombre: nombre,
         descripcion: descripcion,
         direccion: direccion,
@@ -157,9 +177,10 @@ export class NuevoAlojamientoComponent implements OnInit{
         imagenes: images,
       };
 
-      console.log('Datos a enviar al servicio:', alojamientoData);
+      console.log('Datos a enviar al servicio:', alojamientoData); // Log para verificar los datos antes de enviar al servicio
 
-      this.isSubmitting = true;
+      this.isSubmitting = true; // Indica que se está enviando el formulario
+
       this.alojamientoService.verificarAlojamientoExistente(nombre).pipe(
         catchError((error) => {
           console.error(error);
