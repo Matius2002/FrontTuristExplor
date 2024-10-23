fc43e1c4c6cfc13d577247dd001656062dace5b6
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 16:59:12 -0500

    Se agregan comentarios en noticias-contenido.component.spec.ts

--- src/app/Turismo/Noticia/noticias-contenido/noticias-contenido.component.spec.ts
+++ src/app/Turismo/Noticia/noticias-contenido/noticias-contenido.component.spec.ts
@@ -3,21 +3,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { NoticiasContenidoComponent } from './noticias-contenido.component';
 
 describe('NoticiasContenidoComponent', () => {
-  let component: NoticiasContenidoComponent;
-  let fixture: ComponentFixture<NoticiasContenidoComponent>;
+  let component: NoticiasContenidoComponent; // Declaración de la variable del componente
+  let fixture: ComponentFixture<NoticiasContenidoComponent>; // Declaración de la variable para el fixture del componente
 
+  // Configuración inicial antes de cada prueba
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NoticiasContenidoComponent]
+      imports: [NoticiasContenidoComponent] // Importación del componente que se va a probar
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compila los componentes
+
+    // Crea una instancia del componente y del fixture
     fixture = TestBed.createComponent(NoticiasContenidoComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Obtiene la instancia del componente
+    fixture.detectChanges(); // Detecta cambios en el fixture
   });
 
+  // Prueba para verificar que el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Verifica que la instancia del componente sea verdadera (exista)
   });
 });
