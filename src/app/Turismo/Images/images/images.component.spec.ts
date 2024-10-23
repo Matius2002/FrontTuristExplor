f1c4921d75079685cf18e328560f6af8a64dbf12
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 16:31:56 -0500

    Se agregan comentarios en imagenes.component.spec.ts

--- src/app/Turismo/Images/images/images.component.spec.ts
+++ src/app/Turismo/Images/images/images.component.spec.ts
@@ -2,22 +2,28 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ImagesComponent } from './images.component';
 
+// Describe bloque para agrupar las pruebas del componente ImagesComponent
 describe('ImagesComponent', () => {
-  let component: ImagesComponent;
-  let fixture: ComponentFixture<ImagesComponent>;
+  // Declaración de las variables para el componente y su fixture
+  let component: ImagesComponent; // Variable para instanciar el componente
+  let fixture: ComponentFixture<ImagesComponent>; // Fixture para interactuar con el componente
 
+  // Antes de cada prueba
   beforeEach(async () => {
+    // Configuración del módulo de pruebas
     await TestBed.configureTestingModule({
-      imports: [ImagesComponent]
+      imports: [ImagesComponent] // Importar el componente que se va a probar
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compilar los componentes
+
+    // Crear la instancia del componente y su fixture
     fixture = TestBed.createComponent(ImagesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Instanciar el componente
+    fixture.detectChanges(); // Detectar cambios en el componente
   });
 
+  // Prueba para verificar que el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Comprobar que el componente es verdadero (se ha creado)
   });
 });
