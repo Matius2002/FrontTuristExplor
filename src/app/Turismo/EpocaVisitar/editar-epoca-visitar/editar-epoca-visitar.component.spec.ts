d3d57e40ecf1c12a2898891ec25c80181b83159b
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 12:51:27 -0500

    Se agregan comentarios en editar-epoca-visitar.component.spec.ts

--- src/app/Turismo/EpocaVisitar/editar-epoca-visitar/editar-epoca-visitar.component.spec.ts
+++ src/app/Turismo/EpocaVisitar/editar-epoca-visitar/editar-epoca-visitar.component.spec.ts
@@ -3,21 +3,27 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditarEpocaVisitarComponent } from './editar-epoca-visitar.component';
 
 describe('EditarEpocaVisitarComponent', () => {
+  // Declaración de la variable para el componente EditarEpocaVisitar
   let component: EditarEpocaVisitarComponent;
+  // Declaración de la variable para el fixture del componente
   let fixture: ComponentFixture<EditarEpocaVisitarComponent>;
 
+  // Configuración inicial antes de cada prueba
   beforeEach(async () => {
+    // Configura el módulo de pruebas
     await TestBed.configureTestingModule({
-      imports: [EditarEpocaVisitarComponent]
+      imports: [EditarEpocaVisitarComponent] // Importa el componente a probar
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compila los componentes
+
+    // Crea una instancia del componente y del fixture
     fixture = TestBed.createComponent(EditarEpocaVisitarComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Obtiene la instancia del componente
+    fixture.detectChanges(); // Detecta cambios para inicializar el componente
   });
 
+  // Prueba para verificar si el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Verifica que la instancia del componente sea verdadera
   });
 });
