19b76c843d823dba336f09f2babefc71fcafd717
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 12:23:12 -0500

    Se agregan comentarios en editar-atraciones.component.spec.ts

--- src/app/Turismo/AtracionPrincipal/editar-atraciones/editar-atraciones.component.spec.ts
+++ src/app/Turismo/AtracionPrincipal/editar-atraciones/editar-atraciones.component.spec.ts
@@ -3,21 +3,24 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { EditarAtracionesComponent } from './editar-atraciones.component';
 
 describe('EditarAtracionesComponent', () => {
-  let component: EditarAtracionesComponent;
-  let fixture: ComponentFixture<EditarAtracionesComponent>;
+  let component: EditarAtracionesComponent; // Declaración de la variable para el componente
+  let fixture: ComponentFixture<EditarAtracionesComponent>; // Declaración de la variable para el fixture del componente
 
+  // Configuración inicial del entorno de pruebas
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [EditarAtracionesComponent]
+      imports: [EditarAtracionesComponent] // Importación del componente a probar
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compilación de los componentes
+
+    // Creación de una instancia del componente y del fixture
     fixture = TestBed.createComponent(EditarAtracionesComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    component = fixture.componentInstance; // Asignación de la instancia del componente
+    fixture.detectChanges(); // Detección de cambios inicial
   });
 
+  // Prueba para verificar que el componente se crea correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Verificación de que el componente sea verdadero (exista)
   });
 });
