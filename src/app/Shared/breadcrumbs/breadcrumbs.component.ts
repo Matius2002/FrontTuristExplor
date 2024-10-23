04b9c52aa6b1bbaf49366cd6b518851e68497453
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-22 19:14:00 -0500

    Se agregan comentarios en breadcrumbs.component.ts

--- src/app/Shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/Shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,13 +1,16 @@
 import {Component, Input} from '@angular/core';
 
+/**
+ * Componente para mostrar los breadcrumbs (navegación jerárquica) en la aplicación.
+ */
 @Component({
-  selector: 'app-breadcrumbs',
-  standalone: true,
-  imports: [],
-  templateUrl: './breadcrumbs.component.html',
-  styleUrl: './breadcrumbs.component.css'
+  selector: 'app-breadcrumbs', // Selector del componente, se usará en HTML
+  standalone: true, // Indica que este componente se puede usar de forma independiente
+  imports: [], // Se pueden importar otros módulos, si es necesario
+  templateUrl: './breadcrumbs.component.html', // Ruta al archivo de plantilla HTML del componente
+  styleUrls: ['./breadcrumbs.component.css'] // Ruta al archivo de estilos CSS del componente
 })
 export class BreadcrumbsComponent {
-  //@Input() contenidoBreadcrumbs: string | undefined;
-
+  // Propiedad de entrada (input) para recibir contenido desde el componente padre
+  //@Input() contenidoBreadcrumbs: string | undefined; // Esta propiedad se puede usar para pasar datos al componente
 }
