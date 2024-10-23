b5bd00f4c07d131fdf1fc0ac08ae674fda2b5e4d
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 13:07:21 -0500

    Se agregan comentarios en nueva-epoca-visitar.service.spec.ts

--- src/app/Turismo/EpocaVisitar/epoca-visitar.service.spec.ts
+++ src/app/Turismo/EpocaVisitar/epoca-visitar.service.spec.ts
@@ -2,15 +2,18 @@ import { TestBed } from '@angular/core/testing';
 
 import { EpocaVisitarService } from './epoca-visitar.service';
 
+// Descripción de la suite de pruebas para el servicio EpocaVisitarService
 describe('EpocaVisitarService', () => {
-  let service: EpocaVisitarService;
+  let service: EpocaVisitarService; // Declaración de la variable para el servicio
 
+  // Configuración que se ejecuta antes de cada prueba
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(EpocaVisitarService);
+    TestBed.configureTestingModule({}); // Configuración del módulo de prueba
+    service = TestBed.inject(EpocaVisitarService); // Inyección del servicio en la variable
   });
 
+  // Prueba para verificar que el servicio se crea correctamente
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy(); // Afirmación que verifica que el servicio existe
   });
 });
