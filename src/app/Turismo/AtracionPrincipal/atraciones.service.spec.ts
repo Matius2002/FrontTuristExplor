2aad2c03d3199f7ac8ec50caa19233a81b8c19d5
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 12:39:21 -0500

    Se agregan comentarios en atraciones.service.spec.ts

--- src/app/Turismo/AtracionPrincipal/atraciones.service.spec.ts
+++ src/app/Turismo/AtracionPrincipal/atraciones.service.spec.ts
@@ -2,15 +2,18 @@ import { TestBed } from '@angular/core/testing';
 
 import { AtracionesService } from './atraciones.service';
 
+// Describe un conjunto de pruebas para el servicio AtracionesService.
 describe('AtracionesServiceService', () => {
-  let service: AtracionesService;
+  let service: AtracionesService; // Variable para almacenar la instancia del servicio.
 
+  // Configura el entorno de pruebas antes de cada prueba.
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(AtracionesService);
+    TestBed.configureTestingModule({}); // Configura el módulo de pruebas (actualmente vacío).
+    service = TestBed.inject(AtracionesService); // Inyecta la instancia del servicio en la variable service.
   });
 
+  // Prueba para verificar que el servicio se crea correctamente.
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(service).toBeTruthy(); // Verifica que la instancia del servicio no sea nula o indefinida.
   });
 });
