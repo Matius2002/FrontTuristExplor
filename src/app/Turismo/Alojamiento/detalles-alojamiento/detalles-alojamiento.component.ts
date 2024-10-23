5dc543c834fcbe4577ff9e0bceaed993cb454d73
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-23 10:28:59 -0500

    Se agregan comentarios en detalles-alojamiento.component.ts

--- src/app/Turismo/Alojamiento/detalles-alojamiento/detalles-alojamiento.component.ts
+++ src/app/Turismo/Alojamiento/detalles-alojamiento/detalles-alojamiento.component.ts
@@ -1,12 +1,15 @@
 import { Component } from '@angular/core';
 
+// Decorador @Component que define los metadatos del componente
 @Component({
-  selector: 'app-detalles-alojamiento',
-  standalone: true,
-  imports: [],
-  templateUrl: './detalles-alojamiento.component.html',
-  styleUrl: './detalles-alojamiento.component.css'
+  selector: 'app-detalles-alojamiento', // Define el nombre del selector para usar este componente en el HTML
+  standalone: true, // Indica que este componente es independiente y no forma parte de un módulo
+  imports: [], // Aquí se pueden importar otros módulos o componentes si se necesitan
+  templateUrl: './detalles-alojamiento.component.html', // Ruta al archivo HTML que contiene la vista del componente
+  styleUrl: './detalles-alojamiento.component.css' // Ruta al archivo CSS que contiene los estilos del componente
 })
-export class DetallesAlojamientoComponent {
 
+// Definición de la clase del componente
+export class DetallesAlojamientoComponent {
+  // Aquí puedes agregar propiedades y métodos que manejarán la lógica del componente
 }
