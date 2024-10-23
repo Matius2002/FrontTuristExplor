3af7a9b57dbea4a316b95e5f8e20971577b2e885
Mateo Alejandro Mora <mateomora2015@gmail.com>
2024-10-22 23:26:18 -0500

    Se agregan comentarios en sistema.component.spec.ts

--- src/app/Sistema/sistema/sistema.component.spec.ts
+++ src/app/Sistema/sistema/sistema.component.spec.ts
@@ -2,22 +2,29 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SistemaComponent } from './sistema.component';
 
+// Describe el conjunto de pruebas para el 'SistemaComponent'
 describe('SistemaComponent', () => {
-  let component: SistemaComponent;
-  let fixture: ComponentFixture<SistemaComponent>;
+  let component: SistemaComponent; // Declara una variable para el componente que se va a probar
+  let fixture: ComponentFixture<SistemaComponent>; // Declara una variable para el fixture, que es una instancia del componente envuelta en un contenedor de pruebas
 
+  // Configura las pruebas antes de ejecutarlas
   beforeEach(async () => {
+    // Configura el módulo de pruebas de Angular para el 'SistemaComponent'
     await TestBed.configureTestingModule({
-      imports: [SistemaComponent]
+      imports: [SistemaComponent] // Importa el componente en el entorno de pruebas
     })
-    .compileComponents();
-    
+      .compileComponents(); // Compila los componentes antes de ejecutarse
+
+    // Crea una instancia del componente y lo asigna a 'fixture'
     fixture = TestBed.createComponent(SistemaComponent);
+    // Asigna la instancia del componente que se va a probar a la variable 'component'
     component = fixture.componentInstance;
+    // Ejecuta la detección de cambios inicial, necesaria para que el componente reaccione a las propiedades y datos
     fixture.detectChanges();
   });
 
+  // Prueba unitaria que verifica si el componente se creó correctamente
   it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(component).toBeTruthy(); // Verifica si la instancia del componente existe (es decir, si se creó correctamente)
   });
 });
